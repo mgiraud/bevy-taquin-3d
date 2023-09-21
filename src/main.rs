bf2acd694369b60846ac06e28ffb25ab35718f39
Marc <11818237+mgiraud@users.noreply.github.com>
2023-09-21 16:17:02 +0200

    Show frame and rotating tiles

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,120 @@
-use bevy::{prelude::*, render::render_resource::{TextureFormat, TextureDimension, Extent3d}};
+use std::f32::consts::PI;
+
+use bevy::{prelude::*, render::{render_resource::{TextureFormat, TextureDimension, Extent3d}, mesh::VertexAttributeValues}, asset::LoadedFolder};
+use scene_hook::{SceneHook, HookPlugin};
+
+
+mod scene_hook;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Update, rotate)
-        .add_systems(Startup, setup)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(HookPlugin)
+        .add_state::<AppState>()
+        .init_resource::<AppConfig>()
+        .init_resource::<Markers>()
+        .add_systems(OnEnter(AppState::Setup), setup_scene)
+        .add_systems(Update, setup_markers.run_if(in_state(AppState::Setup)))
+        .add_systems(Update, check_setup_finished.run_if(in_state(AppState::Setup)))
+        .add_systems(OnEnter(AppState::SetupTiles), setup_tiles)
+        .add_systems(Update, rotate.run_if(in_state(AppState::Running)))
         .run();
 }
 
-#[derive(Component)]
-struct Shape;
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
+enum AppState {
+    #[default]
+    Setup,
+    SetupTiles,
+    Running,
+}
 
-fn setup(
+#[derive(Resource)]
+struct AppConfig {
+    taquin_size: u8
+}
+
+impl FromWorld for AppConfig {
+    fn from_world(_world: &mut World) -> Self {
+        AppConfig { taquin_size: 4 }
+    }
+}
+
+#[derive(Component)]
+struct FrameScene;
+
+
+#[derive(Resource)]
+struct TaquinSprites {
+    bevy: Handle<Image>,
+    rust: Handle<Image>
+}
+
+struct TaquinSpritesLoaded {
+    bevy: bool,
+    rust: bool
+}
+
+impl TaquinSpritesLoaded {
+    fn is_ready(&self) -> bool {
+        self.bevy && self.rust
+    }
+}
+
+impl FromWorld for TaquinSpritesLoaded {
+    fn from_world(_world: &mut World) -> Self {
+        TaquinSpritesLoaded { bevy: false, rust: false }
+    }
+}
+
+#[derive(Component)]
+struct Marker;
+
+#[derive(Resource, Default)]
+pub struct Markers {
+    pub tl : Option<Vec3>,
+    pub tr : Option<Vec3>,
+    pub bl : Option<Vec3>,
+    pub br : Option<Vec3>,
+}
+
+impl Markers {
+    pub fn is_ready(&self) -> bool {
+        self.tl.is_some() && self.tr.is_some() && self.bl.is_some() && self.br.is_some()
+    }
+}
+
+#[derive(Component)]
+struct Tile;
+
+fn setup_scene(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut images: ResMut<Assets<Image>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
 ) {
-
-
     let debug_material = materials.add(StandardMaterial {
         base_color_texture: Some(images.add(uv_debug_texture())),
         ..default()
     });
 
-
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("models/frame.glb#Scene0"),
-        transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3 { x: 0.1, y: 0.1, z: 0.1 }),
-        ..default()
+    commands.insert_resource(TaquinSprites {
+        bevy: asset_server.load("textures/taquin/bevy.png"),
+        rust: asset_server.load("textures/taquin/rust.png")
     });
+        
+    commands.spawn((SceneBundle {
+        scene: asset_server.load("models/frame.glb#Scene0"),
+        transform: Transform::from_rotation(Quat::from_axis_angle(Vec3::Y, PI)),
+        ..default()
+    }, FrameScene, SceneHook::new(|entity, commands| {
+        match entity.get::<Name>().map(|t|t.as_str()) {
+            Some("TL") | Some("TR") | Some("BL") | Some("BR") => commands.insert(Marker),
+            _ => commands,
+        };
+    })));
+
 
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -46,23 +130,106 @@ fn setup(
     // ground plane
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Plane::from_size(50.0).into()),
-        material: materials.add(Color::SILVER.into()),
+        material: debug_material,
         ..default()
     });
 
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        transform: Transform::from_xyz(0.0, 30., 40.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
         ..default()
     });
-
-    
 }
 
-fn rotate(mut query: Query<&mut Transform, With<Handle<Scene>>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        transform.rotate_y(time.delta_seconds() / 2.);
+fn setup_markers(
+    mut markers: ResMut<Markers>,
+    query: Query<(Entity, &Name, &GlobalTransform), With<Marker>>
+) {
+    for (entity, name, global_transform) in query.iter() {
+        match name.as_str() {
+            "TL" => markers.tl = Some(global_transform.translation()),
+            "TR" => markers.tr = Some(global_transform.translation()),
+            "BL" => markers.bl = Some(global_transform.translation()),
+            "BR" => markers.br = Some(global_transform.translation()),
+            _ => (),
+        };
     }
 }
+
+
+fn check_setup_finished(
+    taquin_sprite_folder: ResMut<TaquinSprites>,
+    mut events: EventReader<AssetEvent<Image>>,
+    mut next_state: ResMut<NextState<AppState>>,
+    mut taquin_sprites_loaded: Local<TaquinSpritesLoaded>,
+    markers: Res<Markers>
+) {
+
+    for event in events.read() {
+        if event.is_loaded_with_dependencies(&taquin_sprite_folder.bevy) {
+            taquin_sprites_loaded.bevy = true;
+        } else if event.is_loaded_with_dependencies(&taquin_sprite_folder.rust) {
+            taquin_sprites_loaded.rust = true;
+        }
+    }
+
+    if markers.is_ready() && taquin_sprites_loaded.is_ready() {
+        next_state.set(AppState::SetupTiles);
+    }
+}
+
+fn setup_tiles(
+    mut commands: Commands,
+    taquin_sprite_handles: Res<TaquinSprites>,
+    markers: Res<Markers>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    app_config : Res<AppConfig>,
+    mut next_state: ResMut<NextState<AppState>>,
+) {
+    let taquin_inner_width = markers.tr.unwrap().x - markers.tl.unwrap().x;
+    let taquin_inner_height = markers.tr.unwrap().y - markers.br.unwrap().y;
+    let tile_width = taquin_inner_width / app_config.taquin_size as f32;
+    let tile_height = taquin_inner_height / app_config.taquin_size as f32;
+    let tile_width_ratio = tile_width / taquin_inner_width;
+    let tile_height_ratio = tile_height / taquin_inner_height;
+    let origin = markers.tl.unwrap();
+    
+    for i in 0..app_config.taquin_size {
+        for j in 1..=app_config.taquin_size {
+            let mut block = Mesh::from(shape::Quad::new(Vec2::new(tile_width, tile_height)));
+            if let Some(attr) = block.attribute_mut(Mesh::ATTRIBUTE_UV_0) {
+                *attr = VertexAttributeValues::Float32x2(vec![
+                    [0. + i as f32 * tile_width_ratio, j as f32 * tile_height_ratio],
+                    [0. + i as f32 * tile_width_ratio, (j - 1) as f32 * tile_height_ratio],
+                    [(i + 1) as f32 * tile_width_ratio, (j - 1) as f32 * tile_height_ratio],
+                    [(i + 1) as f32 * tile_width_ratio, j as f32 * tile_height_ratio],
+                ])
+            }
+            let block_handle = meshes.add(block);
+            commands.spawn((PbrBundle {
+                mesh: block_handle,
+                material: materials.add(StandardMaterial {
+                    base_color_texture: Some(taquin_sprite_handles.bevy.clone()),
+                    double_sided: true,
+                    cull_mode: None,
+                    ..default()
+                }),
+                transform: Transform::from_translation(Vec3 { x: origin.x + i as f32 * tile_width + tile_width / 2. as f32, y: origin.y - j as f32 * tile_height + tile_height / 2., z: 0.75 }),
+                ..default()
+            }, Tile));
+        }
+
+        next_state.set(AppState::Running);
+    }
+}
+
+fn rotate(mut query: Query<(&mut Transform), With<Tile>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds());
+        transform.rotate_x(time.delta_seconds());
+    }
+}
+
 
 /// Creates a colorful test pattern
 fn uv_debug_texture() -> Image {
---
+++ src/scene_hook.rs
@@ -0,0 +1,43 @@
+// FROM https://github.com/nicopap/bevy-scene-hook
+use bevy::{
+    prelude::*,
+    scene::SceneInstance,
+    ecs::{world::EntityRef, system::EntityCommands},
+};
+#[derive(Component, Debug)]
+pub struct SceneHooked;
+
+#[derive(Component)]
+pub struct SceneHook {
+    hook: Box<dyn Fn(&EntityRef, &mut EntityCommands) + Send + Sync + 'static>,
+}
+impl SceneHook {
+    pub fn new<F: Fn(&EntityRef, &mut EntityCommands) + Send + Sync + 'static>(hook: F) -> Self {
+        Self { hook: Box::new(hook) }
+    }
+}
+
+pub fn run_hooks(
+    unloaded_instances: Query<(Entity, &SceneInstance, &SceneHook), Without<SceneHooked>>,
+    scene_manager: Res<SceneSpawner>,
+    world: &World,
+    mut cmds: Commands,
+) {
+    for (entity, instance, hooked) in unloaded_instances.iter() {
+        if scene_manager.instance_is_ready(**instance) {
+            cmds.entity(entity).insert(SceneHooked);
+        }
+        let entities = scene_manager
+            .iter_instance_entities(**instance)
+            .chain(std::iter::once(entity));
+        for entity_ref in entities.filter_map(|e| world.get_entity(e)) {
+            let mut cmd = cmds.entity(entity_ref.id());
+            (hooked.hook)(&entity_ref, &mut cmd);
+        }
+    }
+}
+
+pub struct HookPlugin;
+impl Plugin for HookPlugin {
+    fn build(&self, app: &mut App) { app.add_systems(Update, run_hooks); }
+}
