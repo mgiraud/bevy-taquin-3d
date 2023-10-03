1e9639acc21b5863ee9dc21658ccbfbd3d9ca4f0
Marc <11818237+mgiraud@users.noreply.github.com>
2023-10-03 18:58:24 +0200

    Refactor and Fix solvability again

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::PI;
+use std::{f32::consts::PI, ops::Add};
 
 use bevy::{prelude::*, render::{render_resource::{TextureFormat, TextureDimension, Extent3d}, mesh::VertexAttributeValues}};
 use scene_hook::{SceneHook, HookPlugin};
@@ -12,9 +12,8 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(HookPlugin)
         .add_state::<AppState>()
-        .init_resource::<AppConfig>()
+        .init_resource::<Taquin>()
         .init_resource::<Markers>()
-        .init_resource::<Tiles>()
         .add_systems(OnEnter(AppState::Setup), setup_scene)
         .add_systems(Update, setup_markers.run_if(in_state(AppState::Setup)))
         .add_systems(Update, check_setup_finished.run_if(in_state(AppState::Setup)))
@@ -35,23 +34,15 @@ enum AppState {
 }
 
 #[derive(Resource)]
-struct AppConfig {
-    taquin_size: i8,
-    tiles_nb: i8,
+struct Taquin {
+    size: i8,
+    tiles_nb: usize,
+    tiles: Vec<Vec<Tile>>
 }
 
-impl FromWorld for AppConfig {
+impl FromWorld for Taquin {
     fn from_world(_world: &mut World) -> Self {
-        AppConfig { taquin_size: 2, tiles_nb: 4 }
-    }
-}
-
-#[derive(Resource, Debug)]
-struct Tiles (Vec<Tile>);
-
-impl FromWorld for Tiles {
-    fn from_world(_world: &mut World) -> Self {
-        Tiles(vec![])
+        Taquin { size: 2, tiles_nb: 4, tiles: vec![] }
     }
 }
 
@@ -102,14 +93,43 @@ impl Markers {
 #[derive(Component, Debug)]
 struct EmptyTile;
 
-#[derive(Component, Debug)]
+#[derive(Component, Debug, Clone, Copy, PartialEq, PartialOrd)]
 struct Tile(i8);
 
 #[derive(Component, Debug)]
 struct TileSelected;
 
-#[derive(Component, Debug)]
-struct TileIndex(i8);
+#[derive(Component, Debug, PartialEq, Clone, Copy)]
+struct TilePosition {
+    i: i8,
+    j: i8
+}
+
+impl TilePosition {
+    fn new(i: i8, j:i8) -> Self {
+        Self {i, j}
+    }
+
+    fn is_neighbour_of(&self, other: &TilePosition)-> bool {
+        self.get_neighbours().contains(other)
+    }
+
+    fn get_neighbours(self) -> Vec<TilePosition>
+    {
+        vec![self + (1, 0), self + (0, 1), self + (-1, 0), self + (0, -1)]
+    }
+}
+
+impl Add<(i8, i8)> for TilePosition {
+    type Output = Self;
+
+    fn add(self, other: (i8, i8)) -> Self {
+        Self {
+            i: self.i + other.0,
+            j: self.j + other.1,
+        }
+    }
+}
 
 
 fn setup_scene(
@@ -207,46 +227,40 @@ fn setup_tiles(
     taquin_sprite_handles: Res<TaquinSprites>,
     markers: Res<Markers>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut tiles: ResMut<Tiles>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    app_config : Res<AppConfig>,
+    mut taquin : ResMut<Taquin>,
     mut next_state: ResMut<NextState<AppState>>,
 ) {
     let taquin_inner_width = markers.tr.unwrap().x - markers.tl.unwrap().x;
     let taquin_inner_height = markers.tr.unwrap().y - markers.br.unwrap().y;
-    let tile_width = taquin_inner_width / app_config.taquin_size as f32;
-    let tile_height = taquin_inner_height / app_config.taquin_size as f32;
+    let tile_width = taquin_inner_width / taquin.size as f32;
+    let tile_height = taquin_inner_height / taquin.size as f32;
     let tile_width_ratio = tile_width / taquin_inner_width;
     let tile_height_ratio = tile_height / taquin_inner_height;
     let origin = markers.tl.unwrap();
-    
-    for i in 0..app_config.taquin_size {
-        for j in 1..=app_config.taquin_size {
+
+    taquin.tiles = (0..taquin.size).map(|j| {
+        (0..taquin.size).map(|i| {
             let translation = Vec3 { 
                 x: origin.x + i as f32 * tile_width + tile_width / 2. as f32, 
-                y: origin.y - j as f32 * tile_height + tile_height / 2., 
+                y: origin.y - j as f32 * tile_height - tile_height / 2., 
                 z: 0.75
             };
-            
-            let tile_index = (j - 1) * app_config.taquin_size + i;
-
-            if i == app_config.taquin_size - 1 && j == app_config.taquin_size {
-                commands.spawn((Transform::from_translation(translation), EmptyTile, TileIndex(tile_index)));
-                tiles.0.push(Tile(0));
-                continue;
+            if i == taquin.size - 1 && j == taquin.size - 1 {
+                commands.spawn((Transform::from_translation(translation), EmptyTile, TilePosition::new(i, j)));
+                return Tile(taquin.size * taquin.size);
             }
             let mut block = Mesh::from(shape::Quad::new(Vec2::new(tile_width, tile_height)));
             if let Some(attr) = block.attribute_mut(Mesh::ATTRIBUTE_UV_0) {
                 *attr = VertexAttributeValues::Float32x2(vec![
-                    [0. + i as f32 * tile_width_ratio, j as f32 * tile_height_ratio],
-                    [0. + i as f32 * tile_width_ratio, (j - 1) as f32 * tile_height_ratio],
-                    [(i + 1) as f32 * tile_width_ratio, (j - 1) as f32 * tile_height_ratio],
-                    [(i + 1) as f32 * tile_width_ratio, j as f32 * tile_height_ratio],
-                ])
+                    [0. + i as f32 * tile_width_ratio, (j + 1) as f32 * tile_height_ratio],
+                    [0. + i as f32 * tile_width_ratio, j  as f32 * tile_height_ratio],
+                    [(i + 1) as f32 * tile_width_ratio, j  as f32 * tile_height_ratio],
+                    [(i + 1) as f32 * tile_width_ratio, (j + 1) as f32 * tile_height_ratio],
+                ]);
             }
-            let block_handle = meshes.add(block);
             let mut tile_command = commands.spawn((PbrBundle {
-                mesh: block_handle,
+                mesh: meshes.add(block),
                 material: materials.add(StandardMaterial {
                     base_color_texture: Some(taquin_sprite_handles.bevy.clone()),
                     double_sided: true,
@@ -255,19 +269,19 @@ fn setup_tiles(
                 }),
                 transform: Transform::from_translation(translation),
                 ..default()
-            }, TileIndex(tile_index)));
-            if i == 0 && j == 1 {
+            }, TilePosition::new(i, j)));
+            if i == 0 && j == 0 {
                 tile_command.insert(TileSelected);
             }
-            tiles.0.push(Tile(tile_index + 1))
-        }
-
-        next_state.set(AppState::Running);
-    }
+            Tile(j * taquin.size + i + 1)
+        }).collect()
+    }).collect();
+    
+    next_state.set(AppState::Running);
 }
 
 fn tile_selection_toggle(
-    query: Query<(&Handle<StandardMaterial>, &TileIndex), Changed<TileSelected>>,
+    query: Query<(&Handle<StandardMaterial>, &TilePosition), Changed<TileSelected>>,
     mut materials: ResMut<Assets<StandardMaterial>>
 ) {
     for (material, position) in &query {
@@ -292,11 +306,11 @@ fn react_on_removal(
 }
 
 fn move_tile_selection(
-    empty_tile_query: Query<&TileIndex, (With<EmptyTile>, Without<TileSelected>)>,
-    selected_tile_query: Query<(Entity, &TileIndex), With<TileSelected>>,
-    tiles_query: Query<(Entity, &TileIndex), Without<TileSelected>>,
+    empty_tile_query: Query<&TilePosition, (With<EmptyTile>, Without<TileSelected>)>,
+    selected_tile_query: Query<(Entity, &TilePosition), With<TileSelected>>,
+    tiles_query: Query<(Entity, &TilePosition), Without<TileSelected>>,
     keyboard_input: Res<Input<KeyCode>>,
-    app_config : Res<AppConfig>,
+    taquin : Res<Taquin>,
     mut commands: Commands,
 ) {
     let Ok((selected_tile_entity, selected_tile_index)) = selected_tile_query.get_single() else {
@@ -305,41 +319,39 @@ fn move_tile_selection(
     let Ok(empty_tile_index) = empty_tile_query.get_single() else {
         return;
     };
-    let mut selected_tile_new_position = selected_tile_index.0;
+    let mut selected_tile_new_position = *selected_tile_index;
 
     if keyboard_input.just_released(KeyCode::Left) {
-        selected_tile_new_position -= 1;
-        if selected_tile_new_position == empty_tile_index.0 {
-            selected_tile_new_position -= 1;
+        selected_tile_new_position.i -= 1;
+        if &selected_tile_new_position == empty_tile_index {
+            selected_tile_new_position.i -= 1;
         }
     } else if keyboard_input.just_released(KeyCode::Right) {
-        selected_tile_new_position += 1;
-        if selected_tile_new_position == empty_tile_index.0 {
-            selected_tile_new_position += 1;
+        selected_tile_new_position.i += 1;
+        if &selected_tile_new_position == empty_tile_index {
+            selected_tile_new_position.i += 1;
         }
     } else if keyboard_input.just_released(KeyCode::Up) {
-        selected_tile_new_position -= app_config.taquin_size;
-        if selected_tile_new_position == empty_tile_index.0 {
-            selected_tile_new_position -= app_config.taquin_size;
+        selected_tile_new_position.j -= 1;
+        if &selected_tile_new_position == empty_tile_index {
+            selected_tile_new_position.j -= 1;
         }
     } else if keyboard_input.just_released(KeyCode::Down) {
-        selected_tile_new_position += app_config.taquin_size;
-        if selected_tile_new_position == empty_tile_index.0 {
-            selected_tile_new_position += app_config.taquin_size;
+        selected_tile_new_position.j += 1;
+        if &selected_tile_new_position == empty_tile_index {
+            selected_tile_new_position.j += 1;
         }
     }
 
-    if selected_tile_new_position < 0 {
-        selected_tile_new_position = app_config.taquin_size.pow(2) - ((selected_tile_new_position + 1) % app_config.taquin_size) - 1;
-        if selected_tile_new_position == empty_tile_index.0 {
-            selected_tile_new_position -= 1;
-        }
-    } else if selected_tile_new_position > app_config.taquin_size.pow(2) - 1 {
-        selected_tile_new_position = selected_tile_new_position % app_config.taquin_size;
+    if selected_tile_new_position.i < 0 {
+        selected_tile_new_position.i = taquin.size - 1;
+    }
+    if selected_tile_new_position.j >= taquin.size {
+        selected_tile_new_position.j = 0;
     }
 
     for (tile_entity, tile_position) in tiles_query.iter() {
-        if tile_position.0 == selected_tile_new_position {
+        if tile_position == &selected_tile_new_position {
             commands.entity(selected_tile_entity).remove::<TileSelected>();
             commands.entity(tile_entity).insert(TileSelected);
         }
@@ -347,11 +359,10 @@ fn move_tile_selection(
 }
 
 fn move_selected_tile(
-    mut selected_tile_query: Query<(&mut Transform, &mut TileIndex), (With<TileSelected>, Without<EmptyTile>)>,
-    mut empty_tile_query: Query<(&mut Transform, &mut TileIndex), (With<EmptyTile>, Without<TileSelected>)>,
+    mut selected_tile_query: Query<(&mut Transform, &mut TilePosition), (With<TileSelected>, Without<EmptyTile>)>,
+    mut empty_tile_query: Query<(&mut Transform, &mut TilePosition), (With<EmptyTile>, Without<TileSelected>)>,
     keyboard_input: Res<Input<KeyCode>>,
-    app_config : Res<AppConfig>,
-    mut tiles: ResMut<Tiles>,
+    mut taquin : ResMut<Taquin>,
 ) {
     if !keyboard_input.just_released(KeyCode::Space) {
         return;
@@ -362,72 +373,54 @@ fn move_selected_tile(
     let Ok((mut selected_tile_transform, mut selected_tile_index)) = selected_tile_query.get_single_mut() else {
         return;
     };
-    let diff = empty_tile_index.0 - selected_tile_index.0;
-    let mut diff_checker: Vec<i8> = vec![];
-    if selected_tile_index.0 % app_config.taquin_size != 0 {
-        diff_checker.push(-1);
-    }
-    if selected_tile_index.0 % app_config.taquin_size != app_config.taquin_size - 1 {
-        diff_checker.push(1);
-    }
-    if selected_tile_index.0 + app_config.taquin_size < app_config.tiles_nb {
-        diff_checker.push(app_config.taquin_size);
-    }
-    if selected_tile_index.0 - app_config.taquin_size >= 0 {
-        diff_checker.push(-app_config.taquin_size);
-    }
-    
-    if diff_checker.contains(&diff) {
-        let temp_transform = *empty_tile_transform;
-        *empty_tile_transform = *selected_tile_transform;
-        *selected_tile_transform = temp_transform;
 
-        let temp_index = empty_tile_index.0;
-        empty_tile_index.0 = selected_tile_index.0;
-        selected_tile_index.0 = temp_index;
+    if selected_tile_index.is_neighbour_of(empty_tile_index.as_ref()) {
+        std::mem::swap(empty_tile_transform.as_mut(), selected_tile_transform.as_mut());
+        std::mem::swap(empty_tile_index.as_mut(), selected_tile_index.as_mut());
 
-        tiles.0.swap(selected_tile_index.0 as usize, empty_tile_index.0 as usize);
+        let temp_tile = taquin.tiles[selected_tile_index.j as usize][selected_tile_index.i as usize];
+        taquin.tiles[selected_tile_index.j as usize][selected_tile_index.i as usize] = taquin.tiles[empty_tile_index.j as usize][empty_tile_index.i as usize];
+        taquin.tiles[empty_tile_index.j as usize][empty_tile_index.i as usize] = temp_tile;
     }
 }
 
 fn randomize_tiles(
-    app_config : Res<AppConfig>,
+    mut taquin : ResMut<Taquin>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut tiles: ResMut<Tiles>,
-    mut tiles_query: Query<(&mut Transform, &mut TileIndex, Option<&EmptyTile>)>,
+    mut tiles_query: Query<(&mut Transform, &mut TilePosition, Option<&EmptyTile>)>,
 ) {
     if !keyboard_input.just_released(KeyCode::R) {
         return;
     }
 
     let mut rng = rand::thread_rng();
-    let mut empty_tile_index: Option<i8> = None;
+    let mut empty_tile_position: Option<TilePosition> = None;
     for _i in 0..64 {
-        let n1: usize = rng.gen_range(0..app_config.tiles_nb as usize);
-        let n2: usize = rng.gen_range(0..app_config.tiles_nb as usize);
+        let n1: usize = rng.gen_range(0..taquin.tiles_nb as usize);
+        let n2: usize = rng.gen_range(0..taquin.tiles_nb as usize);
         if n1 == n2 {
             continue;
         }
         let mut tiles_iter = tiles_query.iter_mut();
         if let (Some(mut tile1), Some(mut tile2)) = (tiles_iter.nth(n1), tiles_iter.nth(n2)) {
-            let temp_transform = *(tile1.0);
-            *tile1.0 = *tile2.0;
-            *tile2.0 = temp_transform;
+            std::mem::swap(tile1.0.as_mut(), tile2.0.as_mut());
+            std::mem::swap(tile1.1.as_mut(), tile2.1.as_mut());
     
-            let temp_index = tile1.1.0;
-            tile1.1.0 = tile2.1.0;
-            tile2.1.0 = temp_index;
             if tile1.2.is_some() {
-                empty_tile_index = Some(tile1.1.0)
+                empty_tile_position = Some(tile1.1.to_owned())
             } else if tile2.2.is_some() {
-                empty_tile_index = Some(tile2.1.0)
+                empty_tile_position = Some(tile2.1.to_owned())
             }
-            tiles.0.swap(tile1.1.0 as usize, tile2.1.0 as usize)
+            
+            let temp_tile = taquin.tiles[tile1.1.j as usize][tile1.1.i as usize];
+            taquin.tiles[tile1.1.j as usize][tile1.1.i as usize] = taquin.tiles[tile2.1.j as usize][tile2.1.i as usize];
+            taquin.tiles[tile2.1.j as usize][tile2.1.i as usize] = temp_tile;
         }
     }
 
-    if let Some(index) = empty_tile_index {
-        if !is_solvable(tiles.as_mut(), app_config.as_ref(),index) {
+    println!("EMPTY POSITION : {:?}", empty_tile_position);
+    if let Some(position) = empty_tile_position {
+        if !is_solvable(taquin.as_ref(), position) {
             println!("PAS SOLVABLE");
         } else {
             println!("SOLVABLE");
@@ -436,37 +429,35 @@ fn randomize_tiles(
 }
 
 fn get_inversion_count(
-    tiles: &Tiles,
+    taquin: &Taquin
 ) -> usize
 {
     let mut inversion_counter: usize = 0;
-    let tile_1_iter = tiles.0.iter().take(tiles.0.len() - 1);
-    for (i, tile_1) in tile_1_iter.enumerate() {
-        let tile_index_1 = tile_1.0;
-        for tile_2 in tiles.0.iter().skip(i + 1) {
-            let tile_index_2 = tile_2.0;
-            if tile_index_1 != 0 && tile_index_2 != 0 && tile_index_1 > tile_index_2 {
+    let flat_tiles = taquin.tiles.iter().flatten().collect::<Vec<&Tile>>();
+    (0..(taquin.tiles_nb - 1)).for_each(|i| {
+        ((i + 1)..taquin.tiles_nb).for_each(|j| {
+            println!("COMPARING {:?} > {:?} RESULT {:?}", flat_tiles[i], flat_tiles[j], flat_tiles[i] > flat_tiles[j]);
+            if flat_tiles[i].0 != taquin.tiles_nb as i8 && flat_tiles[j].0 != taquin.tiles_nb as i8 && flat_tiles[i] > flat_tiles[j] {
                 inversion_counter += 1;
             }
-        }
-    }
+        })
+    });
     return inversion_counter;
 }
 
-fn is_solvable<'a>(
-    tiles: &'a Tiles,
-    app_config: &'a AppConfig,
-    empty_tile_index: i8
+fn is_solvable(
+    taquin: &Taquin,
+    empty_tile_position: TilePosition
 ) -> bool {
-    println!("{:?}", tiles);
-    let inversion_count = get_inversion_count(tiles);
+    println!("{:?}", taquin.tiles);
+    let inversion_count = get_inversion_count(taquin);
     println!("Nombre inversion {:?}", inversion_count);
 
-    if app_config.taquin_size & 1 == 1 {
-        return inversion_count & 1 == 0;
+    if taquin.size & 1 == 1 {
+        return empty_tile_position.j & 1 == 0;
     }
 
-    if empty_tile_index & 1 == 1 {
+    if empty_tile_position.j & 1 == 1 {
         return inversion_count & 1 == 0;
     }
 
@@ -505,17 +496,37 @@ fn uv_debug_texture() -> Image {
 mod tests {
     use bevy::prelude::*;
 
-    use crate::{Tile, is_solvable, AppConfig, Tiles};
+    use crate::{Tile, is_solvable, Taquin, TilePosition};
 
     #[test]
     fn test_is_solvable() {
         let mut app = App::new();
 
-        app.world.insert_resource(Tiles(vec![Tile(1), Tile(2), Tile(3), Tile(0)]));
-        app.world.insert_resource(AppConfig {
-            taquin_size: 2,
+        app.world.insert_resource(Taquin {
+            size: 2,
             tiles_nb: 4,
+            tiles: vec![vec![Tile(1), Tile(2)], vec![Tile(3), Tile(4)]]
         });
-        assert_eq!(is_solvable(app.world.resource::<Tiles>(), app.world.resource::<AppConfig>(), 3), true);
+        assert_eq!(is_solvable(app.world.resource::<Taquin>(), TilePosition::new(1, 1)), true);
+
+        app.world.insert_resource(Taquin {
+            size: 2,
+            tiles_nb: 4,
+            tiles: vec![vec![Tile(4), Tile(1)], vec![Tile(2), Tile(3)]]
+        });
+        assert_eq!(is_solvable(app.world.resource::<Taquin>(), TilePosition::new(0, 0)), true);
+    }
+
+    
+    #[test]
+    fn test_is_not_solvable() {
+        let mut app = App::new();
+
+        app.world.insert_resource(Taquin {
+            size: 2,
+            tiles_nb: 4,
+            tiles: vec![vec![Tile(2), Tile(1)], vec![Tile(3), Tile(4)]]
+        });
+        assert_eq!(is_solvable(app.world.resource::<Taquin>(), TilePosition::new(1, 1)), false);
     }
 }
